038ed59735aae8b01918796db53b25ec652ac61b
katamuki <katamuki54@gmail.com>
2025-05-19 09:39:06 +0900

    ZのModtap Shift追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_Q        , KC_W        , KC_E        , KC_R        , KC_T     ,                                   KC_Y     , KC_U        , KC_I     , KC_O     , KC_P     ,
     LGUI_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G     ,                                   KC_H     , RSFT_T(KC_J), KC_K     , KC_L     , RGUI_T(KC_MINS),
-    KC_Z        , KC_X        , KC_C        , KC_V        , KC_B     ,                                   KC_N     , KC_M        , KC_COMM  , KC_DOT   , RSFT_T(KC_SLSH),
+    LSFT_T(KC_Z), KC_X        , KC_C        , KC_V        , KC_B     ,                                   KC_N     , KC_M        , KC_COMM  , KC_DOT   , RSFT_T(KC_SLSH),
     KC_ESC      , KC_LCTL     , KC_LGUI     , KC_LALT     , LT(1,KC_SPC), KC_TAB,        LT(3,KC_BSPC)  , LT(2,KC_ENT), LSFT_T(KC_LNG2), KC_RALT  , KC_RGUI  , KC_RSFT
   ),
 
